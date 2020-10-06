128a5442712690eb3bce51e57faba4c7ec91d06e
ConorDavenport <cnrdavenport@gmail.com>
2020-10-06 22:22:27 +0100

    add routes

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -2,6 +2,16 @@ const express = require('express')
 const router = express.Router()
 const Room = require('../models/Rooms')
 
+router.get('/', (req, res) => {
+  res.send('testing HTTP Requests')
+  console.log('get')
+})
 
+router.put('/', (req, res) => {
+  const roomCode = req.body.roomCode
+  const userName = req.body.userName
+  console.log(`${roomCode}: ${userName}`)
+  res.status(200).end()
+})
 
 module.exports = router
