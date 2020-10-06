6d59968447a2a7fe7dd2d117f58a69d78a1b6127
ConorDavenport <cnrdavenport@gmail.com>
2020-10-06 20:24:20 +0100

    made changes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { w3cwebsocket as W3CWebSocket } from 'websocket'
 import './App.css';
+const axios = require('axios')
 
 // Connect to HTTP server on port 8000
 const client = new W3CWebSocket('ws://localhost:8000')
@@ -10,15 +11,34 @@ export default class App extends React.Component {
     client.onopen = () => {
       console.log('Websocket Client Connected')
     }
-    client.onmessage = () => {
-      const data = JSON.parse(message.data).utf8Data
-      // Do stuff with data
+    client.onmessage = (message) => {
+      const data = message.data
+      console.log(data)
     }
   }
+
+  sendRoomCode() {
+    const roomCode = document.getElementById('roomCode').value
+    this.send(roomCode)
+  }
+
+  send(message) {
+    
+  }
+
   render() {
     return (
       <div className="App">
-      
+        <div>
+          {'Room Code: '}
+          <input id='roomCode' type='text'></input>
+        </div>
+        <div>
+          <button 
+            id='enterRoom'
+            onClick={this.sendRoomCode}
+          >{'Enter Room'}</button>
+        </div>
       </div>
     );
   }
