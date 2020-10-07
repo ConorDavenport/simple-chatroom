a9798b2503efa3c8a53ba0541aefb9629a265377
ConorDavenport <cnrdavenport@gmail.com>
2020-10-07 16:39:18 +0100

    add time function

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,13 +3,17 @@ import { w3cwebsocket as W3CWebSocket } from 'websocket'
 import './App.css';
 import JoinRoom from './components/JoinRoom'
 
+function time() {
+  return (new Date().toLocaleTimeString())
+}
+
 // Connect to HTTP server on port 8000
 const client = new W3CWebSocket('ws://localhost:8000')
 
 export default class App extends React.Component {
   componentDidMount() {
     client.onopen = () => {
-      console.log('Websocket Client Connected')
+      console.log(time() + ': Websocket Client Connected')
     }
     client.onmessage = (message) => {
       const data = message.data
