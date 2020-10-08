965255adc6c3ccd19527004d604af78970c88548
ConorDavenport <cnrdavenport@gmail.com>
2020-10-08 23:36:03 +0100

    add react routes

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,40 +1,24 @@
 import React from 'react';
-import { w3cwebsocket as W3CWebSocket } from 'websocket'
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 import './App.css';
-import JoinRoom from './JoinRoom'
-const axios = require('axios')
-
-function time() {
-  return (new Date().toLocaleTimeString())
-}
-
-// Connect to HTTP server on port 8000
-const client = new W3CWebSocket('ws://localhost:8000')
+import Home from './Home'
+import Room from './Room'
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      uuid: null
-    }
-  }
-  componentDidMount() {
-    client.onopen = () => {
-      console.log(time() + ': Websocket Client Connected')
-    }
-    client.onmessage = (message) => {
-      const data = message.data
-      console.log(data)
-    }
-  }
-
   render() {
     return (
-      <div className="App">
-        <JoinRoom 
-          uuid={this.state.uuid}
-        />
-      </div>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/:room' component={Room} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
