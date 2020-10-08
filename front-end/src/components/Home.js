3014e23a4f0d510ee473b82ec633f2b7a3bfe2fd
ConorDavenport <cnrdavenport@gmail.com>
2020-10-08 23:35:10 +0100

    add home file to tidy things up

---
+++ front-end/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import JoinRoom from './JoinRoom'
+
+export default class Room extends React.Component {
+  render() {
+    return (
+      <div>
+        <JoinRoom />
+      </div>
+    )
+  }
+}
