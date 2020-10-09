e6c447abd285f7e1e2236be79c1527b87372a85f
ConorDavenport <cnrdavenport@gmail.com>
2020-10-09 22:42:14 +0100

    add room database entry

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -1,29 +1,27 @@
 const express = require('express')
 const router = express.Router()
 const Room = require('../models/Rooms')
-const User = require('../models/Users')
 
-router.get('/', (req, res) => {
-  res.send('testing HTTP Requests')
-  console.log('get')
-})
-
-router.put('/', (req, res) => {
+router.put('/request', (req, res) => {
   const roomCode = req.body.roomCode
   const userName = req.body.userName
-  var id
-  const user = new User({
-    name: userName
-  })
-  user.save()
-  .then((data) => {
-    id = data._id
-    console.log(`${userName}:${id} is joining room ${roomCode}`)
-    res.status(200).end()
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+  console.log(`${userName} Requested Room ${roomCode}`)
+
+  if (!Room.exists({ code: roomCode })) {
+    console.log(`Room ${roomCode} doesn't exist`)
+    const room = new Room({ code: roomCode })
+    room.save()
+    .then(() => {
+      res.status(200).end()
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).end()
+    })
+  }
+  else {
+    res.status(200).end()  
+  }
 })
 
 module.exports = router
