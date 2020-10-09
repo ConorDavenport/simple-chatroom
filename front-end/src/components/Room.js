b35a07a1ef2a39e461fc093165163a4e2285e267
ConorDavenport <cnrdavenport@gmail.com>
2020-10-09 22:40:50 +0100

    add websocket stuff

--- front-end/src/components/Room.js
+++ front-end/src/components/Room.js
@@ -1,11 +1,44 @@
 import React from 'react'
+import { w3cwebsocket as W3CWebSocket } from 'websocket'
 
+var client, roomCode, id
+   
 export default class Room extends React.Component {
+  componentDidMount() {
+    client = new W3CWebSocket('ws://localhost:8000')
+    client.onopen = () => {
+      console.log('WebSocket Connected')
+    }
+    client.onmessage = (message) => {
+      var data = message.data
+      try {
+        data = JSON.parse(data)
+        if (data.type === 'setup') {
+          id = data.id
+          client.send({
+            type: 'setup'
+          })
+        }
+      } catch {
+        console.log(data)
+      }
+    }
+  }
+
+  handleClick() {
+    const message = document.getElementById('message').value
+    client.send(message)
+  }
+
   render() {
-    const roomCode = window.location.href.match(/\/\w*$/)[0].substring(1)
+    roomCode = window.location.href.match(/\/:\w*$/)[0].substring(2)
     return (
       <div>
-        {`This is room ${roomCode}`}
+        <div>{`This is room ${roomCode}`}</div>
+        <div>
+          <input type='text' id='message'/>
+          <button onClick={this.handleClick}>Submit</button>
+        </div>
       </div>
     )
   }
