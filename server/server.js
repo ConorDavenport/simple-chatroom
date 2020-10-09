d08ada706dcc6dcbc610d46327d36dda67ee22be
ConorDavenport <cnrdavenport@gmail.com>
2020-10-09 22:41:53 +0100

    made changes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const http = require('http')
 const WebSocket = require('ws')
 const User = require('./models/Users')
+const Room = require('./models/Rooms')
 const cors = require('cors')
 require('dotenv/config')
 
@@ -22,39 +23,43 @@ app.use(cors()) // allow api calls from any ip
 
 // import routes
 const roomsRoutes = require('./routes/rooms')
-app.use('/', roomsRoutes)
+
+app.use('/rooms', roomsRoutes)
 
 const server = http.createServer(app)
+
+//------------------------------------//
+//      WEBSOCKET INTIALISATION       //
+//------------------------------------//
 const wss = new WebSocket.Server({ server })
 // wss = websocket server, represents the backend
 // ws = websocket, represents the frontend
 
-// when a client connects to the websocket server
-// add that user to the Users database and send
-// the client's uuid to the client
 wss.on('connection', (ws) => {
+  // when a client connects to the websocket server
+  // add that user to the Users database and send
+  // the client's uuid to the client
   var id
-  const user = new User({
-    name: 'User'
-  })
+  const user = new User()
   user.save()
   .then((data) => {
     id = data._id
     console.log(time() + `: ${id} Connected`)
-    ws.send(JSON.stringify(id))
+    const initMessage = {
+      type: 'setup',
+      id: id
+    }
+    ws.send(JSON.stringify(initMessage))
   })
 
   //------ EVENT HANDLERS ------
   ws.on('message', (message) => {
-    console.log(message)
+    console.log(`${time()}: ${id}: ${message}`)
   })
   
   ws.on('close', () => {
     console.log(time() + `: ${id} Disconnected`)
     User.deleteOne({_id: id})
-    .then(() => {
-      console.log(time() + `: ${id} Deleted`)
-    })
     .catch((err) => {
       console.log(err)
     })
@@ -74,9 +79,8 @@ mongoose.connect(process.env.DB_CONNECTION,
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => { 
     console.log('Connected to Database')
-    User.deleteMany({}, () => {
-      console.log('Cleared Database')
-    })
+    User.deleteMany({}).catch((err) => console.log(err))
+    Room.deleteMany({}).catch((err) => console.log(err))
   }  
 )
 
