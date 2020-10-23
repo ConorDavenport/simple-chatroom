9e667178c7525fa160c91120a20be803a2ac1362
ConorDavenport <cnrdavenport@gmail.com>
2020-10-23 19:46:04 +0100

    add routes and pass props to routes

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -9,13 +9,31 @@ import Home from './Home'
 import Room from './Room'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: 'User',
+      messages: [],
+    }
+  }
+
   render() {
     return (
       <Router>
         <div className="App">
           <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/:room' component={Room} />
+            <Route 
+              exact path='/'
+              render={(props) => (
+                <Home {...props} 
+                  name={this.state.name} />
+              )} />
+            <Route 
+              exact path='/chat-room'
+              render={(props) => (
+                <Room {...props} 
+                  name={this.state.name} />
+              )} />
           </Switch>
         </div>
       </Router>
