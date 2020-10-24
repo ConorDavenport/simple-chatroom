b196766705d660c2f2f5773b791adcc2238bdfef
ConorDavenport <cnrdavenport@gmail.com>
2020-10-24 17:29:18 +0100

    refactor

--- front-end/src/components/Room.js
+++ front-end/src/components/Room.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { w3cwebsocket as W3CWebSocket } from 'websocket'
+import Feed from './Feed'
 
-var client, roomCode, id
+var client
    
 export default class Room extends React.Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+  }
+
   componentDidMount() {
     client = new W3CWebSocket('ws://localhost:8000')
     client.onopen = () => {
@@ -11,34 +17,25 @@ export default class Room extends React.Component {
     }
     client.onmessage = (message) => {
       var data = message.data
-      try {
-        data = JSON.parse(data)
-        if (data.type === 'setup') {
-          id = data.id
-          client.send({
-            type: 'setup'
-          })
-        }
-      } catch {
-        console.log(data)
-      }
+      console.log(data)
     }
   }
 
   handleClick() {
     const message = document.getElementById('message').value
-    client.send(message)
+    client.send({
+      message: message,
+      user: this.props.name
+    })
   }
 
   render() {
-    roomCode = window.location.href.match(/\/:\w*$/)[0].substring(2)
     return (
       <div>
-        <div>{`This is room ${roomCode}`}</div>
-        <div>
-          <input type='text' id='message'/>
-          <button onClick={this.handleClick}>Submit</button>
-        </div>
+        <div>{`Hello ${this.props.name}`}</div>
+        <input type='text' id='message'/>
+        <button onClick={this.handleClick}>Submit</button>
+        <Feed />
       </div>
     )
   }
