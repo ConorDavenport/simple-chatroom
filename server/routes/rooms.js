122ea83a875352e86b2396b22e8011e9d9d58b18
ConorDavenport <cnrdavenport@gmail.com>
2020-10-24 17:26:24 +0100

    add start of previous message get request

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -1,27 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const Room = require('../models/Rooms')
+const Messages = require('../models/Messages')
 
-router.put('/request', (req, res) => {
-  const roomCode = req.body.roomCode
-  const userName = req.body.userName
-  console.log(`${userName} Requested Room ${roomCode}`)
-
-  if (!Room.exists({ code: roomCode })) {
-    console.log(`Room ${roomCode} doesn't exist`)
-    const room = new Room({ code: roomCode })
-    room.save()
-    .then(() => {
-      res.status(200).end()
-    })
-    .catch((err) => {
-      console.log(err)
-      res.status(500).end()
-    })
-  }
-  else {
-    res.status(200).end()  
-  }
+// client requesting messages
+router.get('/messages', (req, res) => {
+  const recent = req.query.recent
+  res.send(recent)
+  res.status(200).end()
 })
 
 module.exports = router
