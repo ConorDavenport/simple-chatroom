03a9646ef9aae75ac9da2acf06f4ebd16628d8db
ConorDavenport <cnrdavenport@gmail.com>
2020-10-28 21:24:48 +0000

    get messages when client requests them

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -2,11 +2,27 @@ const express = require('express')
 const router = express.Router()
 const Messages = require('../models/Messages')
 
-// client requesting messages
+// client requesting older messages
+// client sends the id of the least recent
+// message they recieved and the server
+// responds with the previous messages
 router.get('/messages', (req, res) => {
-  const recent = req.query.recent
-  res.send(recent)
-  res.status(200).end()
+  const leastRecent = req.query.leastRecent
+  if (leastRecent === '0') {
+    Messages.find({}).sort({ _id: 'descending'})
+    .then((docs) => {
+      console.log(docs)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  } else {
+    Messages.findById(leastRecent, (err, docs) => {
+      console.log(docs)
+      res.send(docs)
+      res.status(200).end()
+    })
+  }
 })
 
 module.exports = router
