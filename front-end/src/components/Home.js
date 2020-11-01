4bf035be79c67d97f16ef13a4bf4dd86005113c8
ConorDavenport <cnrdavenport@gmail.com>
2020-11-01 16:14:18 +0000

    update styling

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -4,7 +4,7 @@ import JoinRoom from './JoinRoom'
 export default class Room extends React.Component {
   render() {
     return (
-      <div>
+      <div id='home'>
         <JoinRoom 
           changeName={this.props.changeName}/>
       </div>
