fa4a171da88f50c9267eb094b57b3060da3253f3
Conor <43864112+ConorDavenport@users.noreply.github.com>
2020-11-01 15:44:32 +0000

    Updates

---
+++ precache-manifest.7c85e7afc44ab69095ba4244011026c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79ee8cdb8bb55cb750845095ba822da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3dd6c8f91739b3e3fe4",
+    "url": "/static/css/main.53fc8c5a.chunk.css"
+  },
+  {
+    "revision": "a6345b527eef077d6335",
+    "url": "/static/js/2.8f60f39f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8f60f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3dd6c8f91739b3e3fe4",
+    "url": "/static/js/main.93969877.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  }
+]);
