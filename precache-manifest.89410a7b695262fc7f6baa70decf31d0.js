ccd948bac6e64b844465c0ea8c72990271ffe82b
Conor <43864112+ConorDavenport@users.noreply.github.com>
2020-11-02 21:40:52 +0000

    Updates

---
+++ precache-manifest.89410a7b695262fc7f6baa70decf31d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965caf8aaf556cb1e16d8cda3262e219",
+    "url": "/simple-chatroom/index.html"
+  },
+  {
+    "revision": "fb9e9d53cdd90b1d001a",
+    "url": "/simple-chatroom/static/css/main.5d62bf1a.chunk.css"
+  },
+  {
+    "revision": "a6345b527eef077d6335",
+    "url": "/simple-chatroom/static/js/2.8f60f39f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-chatroom/static/js/2.8f60f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb9e9d53cdd90b1d001a",
+    "url": "/simple-chatroom/static/js/main.443a0778.chunk.js"
+  },
+  {
+    "revision": "15072ed4b2b21e371d4f",
+    "url": "/simple-chatroom/static/js/runtime-main.72fedb6e.js"
+  }
+]);
